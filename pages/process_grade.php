321226a52b2e280c9b97260962ad4f415728c85e
Nnaji <n.richard@genesystechhub.com>
2019-09-03 17:11:00 +0100

    changes

--- pages/process_grade.php
+++ pages/process_grade.php
@@ -1,13 +1,18 @@
 <?php
     include_once'dbconnection/connection.php';
-?>
-<?php
     if(isset($_POST['submit'])){
-        $applicant=$_REQUEST['id'];
-        $judge=$_REQUEST['judge'];
-        $score=($_POST['rating']);
 
-        
+    $applicant=$_REQUEST['id'];
+    $judge=$_REQUEST['judge'];
+    $score=($_POST['rating']);
+
+    $query = "SELECT *
+    FROM score_table WHERE judge_id = $judge AND applicant_id = $applicant ";
+    $result = mysqli_query($conn,$query);
+   
+    $rowcount = mysqli_num_rows($result);
+    if($rowcount < 1)
+    {
         $sql = "INSERT INTO score_table (applicant_id, judge_id, score)
         VALUES ('$applicant', '$judge', '$score')";
 
@@ -17,6 +22,12 @@
         } else {
             echo "Error: " . $sql . "<br>" . mysqli_error($conn);
         }
+    }else{
+       
+        echo "Applicant have already been graded";
+        header("Location: applicants.php");
+        
+    }
           
     }
 ?>
