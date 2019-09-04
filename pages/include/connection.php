f71c376facdf4af3f191fbe700c4cc680db7fc7b
Ogbonna Vitalis <agavitalisogbonna@gmail.com>
2019-09-04 02:13:27 +0100

    final edits

--- pages/include/connection.php
+++ pages/include/connection.php
@@ -1,13 +1,13 @@
 <?php
-$servername="localhost";
-$username="root";
-$password="";
-$dbName="ignite_applicant";
+// $servername="localhost";
+// $username="root";
+// $password="";
+// $dbName="ignite_applicant";
 
-// $servername = "us-cdbr-iron-east-02.cleardb.net";
-// $username = "b8d16b8c0b7b66";
-// $password = "aefcbeb4";
-// $dbName = "heroku_be5771159a4ff36";
+$servername = "us-cdbr-iron-east-02.cleardb.net";
+$username = "b8d16b8c0b7b66";
+$password = "aefcbeb4";
+$dbName = "heroku_be5771159a4ff36";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbName);
